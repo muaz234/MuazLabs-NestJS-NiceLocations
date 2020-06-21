8fbeb7163df5707f882f1b9dc63f1c6060d16535
muaz234 <ahmedmuaz0152@gmail.com>
2020-06-22 01:09:31 +0800

    db string values

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,14 @@
 import { resolve, join } from 'path';
 
+const SOURCE_PATH = resolve(__dirname, '..', 'src');
 export default {
+    database: {
+        url: 'postgress://app:password@localhost/locations'
+    },
+    orm: {
+        entities: [`${SOURCE_PATH}/**/*.entity.ts`],
+        synchronize: false
+    },
     templates: {
         path: resolve(__dirname, '..', 'views')
     },
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from 'src/config/config.module';
+import { PostgresTypeOrmConfigService } from './postgres-typeorm-config';
 
-@Module({})
+@Module({
+
+    imports: [
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useClass: PostgresTypeOrmConfigService
+        })
+    ]
+})
 export class DatabaseModule {}
